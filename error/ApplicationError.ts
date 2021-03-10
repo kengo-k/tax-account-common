546e7befc6c621163812a683d0dee8fb16e98bf2
kurobanekengo <kurobanekengo@gmail.com>
2021-03-10 19:12:56 +0900

    RequestError追加

--- error/ApplicationError.ts
+++ error/ApplicationError.ts
@@ -1,3 +1,13 @@
 export abstract class ApplicationError extends Error {
   public abstract HTTP_CODE: number;
+  public abstract ERROR_MESSAGE: string;
+}
+
+export class RequestError extends ApplicationError {
+  public HTTP_CODE = 400;
+  public ERROR_MESSAGE;
+  public constructor(message: string) {
+    super();
+    this.ERROR_MESSAGE = message;
+  }
 }

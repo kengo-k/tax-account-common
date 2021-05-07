cf7cf2c905305e1ad33a33dbda009df72c0a8665
kurobanekengo <kurobanekengo@gmail.com>
2021-05-07 16:01:50 +0900

    constant: 細目コード定数を整備

--- constant/saimoku.ts
+++ constant/saimoku.ts
@@ -1,7 +1,12 @@
 export const SaimokuCodeConst = {
   CASH: "A11", // 現金
-  UNPAID_SALARY: "B11", // 未払い給与
+  DEPOSIT: "A21", // 預金(郵貯),
+  URIKAKE: "A31", // 売掛金
+  UNPAID_SALARY: "B11", // 未払給与
+  UNPAID_EXPENSE: "B12", // 未払経費
+  DEBT: "B21", // 借入金
   SALES: "D11", // 売上
-  MISC: "E62", // 雑費
   SALARY: "E11", //役員報酬
+  RENT: "E51", // 家賃
+  MISC: "E62", // 雑費
 };

baaff333f25fe5f91457489d83765417731e8242
kurobanekengo <kurobanekengo@gmail.com>
2021-05-15 12:21:47 +0900

    台帳検索リクエストに月を追加

--- model/journal/LedgerSearchRequest.ts
+++ model/journal/LedgerSearchRequest.ts
@@ -3,11 +3,13 @@ import { Converter, ConverterItem } from "@common/Converter";
 export interface ILedgerSearchRequest {
   nendo: string;
   ledger_cd: string;
+  month?: string;
 }
 
 export class LedgerSearchRequest implements ILedgerSearchRequest {
   public nendo: string;
   public ledger_cd: string;
+  public month: string | undefined;
   constructor(
     initialValues: Partial<ILedgerSearchRequest> | undefined = undefined
   ) {
@@ -18,6 +20,7 @@ export class LedgerSearchRequest implements ILedgerSearchRequest {
     const values = anyValues as ILedgerSearchRequest;
     this.nendo = values.nendo;
     this.ledger_cd = values.ledger_cd;
+    this.month = values.month;
   }
 
   public static isValid(json: any) {
@@ -25,6 +28,7 @@ export class LedgerSearchRequest implements ILedgerSearchRequest {
     const { add } = converter;
     add("nendo", ConverterItem.String, true, false);
     add("ledger_cd", ConverterItem.String, true, false);
+    add("month", ConverterItem.String, false, false);
     return converter.convert(json);
   }
 }
